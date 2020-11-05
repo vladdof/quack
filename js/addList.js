03a102240dca52c925975cb5baca34aea904b9a0
Vlad <vladislav.efanov@gmail.com>
2020-11-05 21:32:20 +0100

    Отформатировал код.

--- js/addList.js
+++ js/addList.js
@@ -1,134 +1,137 @@
 window.onload = function() {
 
 	let inputName = document.querySelector('.name-auto'),
-		 inputYear = document.querySelector('.year-auto'),
-	   inputPrice = document.querySelector('.price-auto'),
-	     inputDes = document.querySelector('.description-auto'),
-	   inputRadio = document.getElementsByName('color'),
-	       select = document.getElementById('select'),
-	      btnSend = document.querySelector('.choose-btn'),
-	        tabel = document.querySelector('.wrap-tabel');
+		inputYear = document.querySelector('.year-auto'),
+	   	inputPrice = document.querySelector('.price-auto'),
+		inputDes = document.querySelector('.description-auto'),
+	   	inputRadio = document.getElementsByName('color'),
+		select = document.getElementById('select'),
+		btnSend = document.querySelector('.choose-btn'),
+		tabel = document.querySelector('.wrap-tabel');
 
 	function createEl() {
 
 		let listAll = document.createElement('div'),
-		      descr = document.createElement('div'),
-		  descrSpan = document.createElement('span'),
-		       year = document.createElement('div'),
-		  colorWrap = document.createElement('div'),
-		      color = document.createElement('div'),
-		     status = document.createElement('div'),
-		      price = document.createElement('div'),
-		    btnWrap = document.createElement('div'),
-		     btnDel = document.createElement('button');
+			descr = document.createElement('div'),
+		  	descrSpan = document.createElement('span'),
+			year = document.createElement('div'),
+		  	colorWrap = document.createElement('div'),
+			color = document.createElement('div'),
+			status = document.createElement('div'),
+			price = document.createElement('div'),
+			btnWrap = document.createElement('div'),
+			btnDel = document.createElement('button');
 
-		   listAll.classList.add('list-all');
-		   descr.classList.add('list-description');
-		   year.classList.add('list-year');
-		   colorWrap.classList.add('list-color');
-		   color.classList.add('list-color_w');
-		   status.classList.add('list-status');
-		   price.classList.add('list-price');
-		   btnWrap.classList.add('list-btn');
-		   btnDel.classList.add('list-btn_del');
+		   	listAll.classList.add('list-all');
+			descr.classList.add('list-description');
+			year.classList.add('list-year');
+			colorWrap.classList.add('list-color');
+			color.classList.add('list-color_w');
+			status.classList.add('list-status');
+			price.classList.add('list-price');
+			btnWrap.classList.add('list-btn');
+			btnDel.classList.add('list-btn_del');
 
-		   // заполнил название
-		   descr.innerHTML = inputName.value + "<br>";
-		   if (inputName.value == '') {
-		     	return alert("Введите название");
-		   } else {
-		     	listAll.appendChild(descr);
-		   }
+			// заполнил название
+			descr.innerHTML = inputName.value + '<br>';
+			if (inputName.value == '') {
+				return alert('Введите название');
+			} else {
+				listAll.appendChild(descr);
+			}
 
-		   // заполнил год
-		   year.textContent = inputYear.value;
-		   if (inputYear.value == '') {
-		   	return alert("Введите год");
-		   } else if (inputYear.value.length != 4) {
-		     	return alert("Введите год в формате 2010");
-		   } else {
-		     	listAll.appendChild(year);
-		   }
-		     
-		   // заполнили цвет
-		   let checked = document.querySelector('input:checked');
-  			if (checked) {
-  			  let b = checked.value;
-  			  color.classList.add(`list-color_${b}`);
-  			} else {
-  				return alert("Укажите цвет");
-  			}
-		   listAll.appendChild(colorWrap);
-		   colorWrap.appendChild(color);
+			// заполнил год
+			year.textContent = inputYear.value;
+			if (inputYear.value == '') {
+				return alert('Введите год');
+			} else if (inputYear.value.length != 4) {
+				return alert('Введите год в формате 2010');
+			} else {
+				listAll.appendChild(year);
+			}
 
-		   // заполнили статус
-		   if (select.value == 'Статус') {
-		   	return alert("Введите статус");
-		   } else {
-		   	status.textContent = select.value;
-		   	listAll.appendChild(status);
-		   }
+			// заполнили цвет
+			let checked = document.querySelector('input:checked');
+			if (checked) {
+			  let b = checked.value;
+			  color.classList.add(`list-color_${b}`);
+			} else {
+				return alert('Укажите цвет');
+			}
+			listAll.appendChild(colorWrap);
+			colorWrap.appendChild(color);
 
-		   // цена авто
-		   price.innerHTML = inputPrice.value + " руб.";
-		   if (inputPrice.value == '') {
-		     	return alert("Введите цену");
-		   } else {
-		     	listAll.appendChild(price);
-		   }
+			// заполнили статус
+			if (select.value == 'Статус') {
+				return alert('Введите статус');
+			} else {
+				status.textContent = select.value;
+				listAll.appendChild(status);
+			}
 
-		   // если есть описание (span element)
-		   if (inputDes.value != 0) {
-		     	descrSpan.innerHTML = "+ " + inputDes.value;
-		     	descr.appendChild(descrSpan);
-		   }
+			// цена авто
+			price.innerHTML = inputPrice.value + ' руб.';
+			if (inputPrice.value == '') {
+				return alert('Введите цену');
+			} else {
+				listAll.appendChild(price);
+			}
 
-		   // btn-del
-		   btnDel.textContent = "Удалить";
-		   listAll.appendChild(btnWrap);
-		   btnWrap.appendChild(btnDel);
+			// если есть описание (span element)
+			if (inputDes.value != 0) {
+				descrSpan.innerHTML = '+ ' + inputDes.value;
+				descr.appendChild(descrSpan);
+			}
 
-	      // создал таблицу
-	      tabel.appendChild(listAll);
+			// btn-del
+			btnDel.textContent = 'Удалить';
+			listAll.appendChild(btnWrap);
+			btnWrap.appendChild(btnDel);
 
-		   setTimeout(function() {
-		      let firstLine = document.querySelectorAll('.first-line input'),
-		      	secondLine = document.querySelector('.description-auto');
-		      for (let i = 0; i < firstLine.length; i++) {
-		        firstLine[i].value = '';
-		      }
-		      secondLine.value = '';
-		      if (select.value != 'Статус') {
-		      	select.selectedIndex = 0;
-		      }
-		      for ( let i = 0; i < inputRadio.length; i++) {
-		      	inputRadio[i].checked = false;
-		      }
-		   }, 10);
+			// создал таблицу
+			tabel.appendChild(listAll);
 
-	      // btn del
-	      let clickDel = document.querySelectorAll('.list-btn_del');
-	      for (let i = 0; i < clickDel.length; i++) {
-		     	clickDel[i].onclick = () => {
-		     		let wrapBtn = document.querySelectorAll('.list-btn')[i],
-		     		 parentWrap = wrapBtn.parentNode;
-		     		     // tabel1 = document.querySelector('.wrap-tabel');
-		     		// tabel1.removeChild(parentWrap);
-		     		parentWrap.style.display = "none";
-		     	}
-	      }
+			setTimeout(function() {
+				let firstLine = document.querySelectorAll('.first-line input'),
+					secondLine = document.querySelector('.description-auto');
+
+				for (let i = 0; i < firstLine.length; i++) {
+					firstLine[i].value = '';
+				}
+
+				secondLine.value = '';
+				if (select.value != 'Статус') {
+					select.selectedIndex = 0;
+				}
+
+				for ( let i = 0; i < inputRadio.length; i++) {
+					inputRadio[i].checked = false;
+				}
+			}, 10);
+
+			// btn del
+			let clickDel = document.querySelectorAll('.list-btn_del');
+			for (let i = 0; i < clickDel.length; i++) {
+				clickDel[i].onclick = () => {
+					let wrapBtn = document.querySelectorAll('.list-btn')[i],
+					 	parentWrap = wrapBtn.parentNode;
+
+					parentWrap.style.display = 'none';
+				}
+			}
 	};
-	
+
 	btnSend.addEventListener('click', function() {
 		createEl();
 	});
 
 	let del = document.querySelectorAll('.list-btn_del');
 	for (let i = 0; i < del.length; i++) {
-     	del[i].onclick = () => {
-     		let wrapBtn = document.querySelectorAll('.list-btn')[i],
-     		 parentWrap = wrapBtn.parentNode;
-     		parentWrap.style.display = "none";
-     	}
+		del[i].onclick = () => {
+			let wrapBtn = document.querySelectorAll('.list-btn')[i],
+				parentWrap = wrapBtn.parentNode;
+
+			parentWrap.style.display = 'none';
+		}
 	}
 }
--- js/field.js
+++ js/field.js
@@ -1,34 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$('.name-auto').on({
-		click: function(){
-	   		$('#name-auto').css({"top":"-30%","display":"block"})
-	   	},
-	   	blur: function(){
-	   		$('#name-auto').css({"top":"50%","display":"none"})
-	   	}
-	});
-	$('.year-auto').on({
-		click: function(){
-	   		$('#year-auto').css({"top":"-30%","display":"block"})
+		click: function() {
+	   		$('#name-auto').css({'top':'-30%','display':'block'});
 	   	},
 	   	blur: function() {
-	   		$('#year-auto').css({"top":"50%","display":"none"})
+	   		$('#name-auto').css({'top':'50%','display':'none'});
 	   	}
 	});
+
+	$('.year-auto').on({
+		click: function() {
+	   		$('#year-auto').css({'top':'-30%','display':'block'});
+	   	},
+	   	blur: function() {
+	   		$('#year-auto').css({'top':'50%','display':'none'});
+	   	}
+	});
+
 	$('.price-auto').on({
-		click: function(){
-	   		$('#price-auto').css({"top":"-30%","display":"block"})
+		click: function() {
+	   		$('#price-auto').css({'top':'-30%','display':'block'});
 	   	},
-	   	blur: function(){
-	   		$('#price-auto').css({"top":"50%","display":"none"})
+	   	blur: function() {
+	   		$('#price-auto').css({'top':'50%','display':'none'});
 	   	}
 	});
+
 	$('.description-auto').on({
-		click: function(){
-	   		$('#description-auto').css({"top":"-30%","display":"block"})
+		click: function() {
+	   		$('#description-auto').css({'top':'-30%','display':'block'});
 	   	},
-	   	blur: function(){
-	   		$('#description-auto').css({"top":"50%","display":"none"})
+	   	blur: function() {
+	   		$('#description-auto').css({'top':'50%','display':'none'});
 	   	}
 	});
 });
